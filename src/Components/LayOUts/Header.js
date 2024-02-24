c53a2c9caa27470b3496bec8404c21c412d9d616
HP <gdikshan10@gmail.com>
2024-02-24 16:32:05 +0545

    bootstrap.min.js file imported for hamburger menu

--- src/Components/LayOUts/Header.js
+++ src/Components/LayOUts/Header.js
@@ -14,39 +14,45 @@ export const Header = () => {
           <Link to="/" className='w-14' >
             <img className='img-fluid' src={navbrand} alt='navbrand'></img>
           </Link>
-        <form className="d-flex">
-            <div className="input-group">
-              <input className="form-control px-4 round-s" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-danger px-4 round-e " type="submit">
-              <GoSearch style={{ color:'whitesmoke' }} />
-              </button>
-            </div>
-          </form>
-        
-          <ul className="navbar-nav mb-2 mb-lg-0 d-flex justify-content-between gap-3 me-3">
-            <li className="nav-item">
-              <NavLink className="nav-link"  to="/movie">Movies</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about">About Us</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/Login">
-                <FaUserCircle style={{ fontSize: '1.7em' }} />
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link position-relative" to="/favourite">
-                <FaHeart style={{ fontSize: '1.5em'}} />
-                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                  99+
-                </span>
-              </NavLink>
-            </li>
-          </ul>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+            <form className="d-flex">
+              <div className="input-group">
+                <input className="form-control px-4 round-s" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-danger px-4 round-e " type="submit">
+                <GoSearch style={{ color:'whitesmoke' }} />
+                </button>
+              </div>
+            </form>
+          
+            <ul className="navbar-nav mb-2 mb-lg-0 d-flex justify-content-between gap-3 me-3">
+              <li className="nav-item">
+                <NavLink className="nav-link"  to="/movie">Movies</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">About Us</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/Login">
+                  <FaUserCircle style={{ fontSize: '1.7em' }} />
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link position-relative" to="/favourite">
+                  <FaHeart style={{ fontSize: '1.5em'}} />
+                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    99+
+                  </span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
       </div>
     </nav>
   )
