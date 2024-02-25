724fc3e3c0f382ecc46154d2c88b09a63ee29684
HP <gdikshan10@gmail.com>
2024-02-25 13:14:41 +0545

    banner part completed

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -3,6 +3,11 @@ import {Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 import './Banner.css';
 import {Movies} from '../../Data/MovieData';
+import { FcRating } from "react-icons/fc";
+import { MdOutlineDateRange } from "react-icons/md";
+import { BiSolidCategory } from "react-icons/bi";
+import { FaRegPlayCircle } from "react-icons/fa";
+
 export const Banner = () => {
   return (
     <section>
@@ -19,7 +24,25 @@ export const Banner = () => {
                         {Movies.map((item,index)=>(
                             <SwiperSlide key={index} className='slide-h'>
                                 <img alt={item.name} className='img-fluid' src={item.image} />
-                                <div className='overlay'></div>
+                                <div className='overlay d-flex align-items-center '>
+                                    <div className='container-fluid ps-sm-0 ps-md-5'>
+                                        <p>{item.description}</p>
+                                        <h1 className='fs-3 fw-bold' > {item.name}</h1>
+                                        <p>{item.desc}</p>
+                                        <p className="border rounded-pill d-inline py-2 px-4 me-2">
+                                            <BiSolidCategory style={{marginRight:'.3rem'}} />
+                                            {item.category}</p>
+                                        <p className="border rounded-pill d-inline py-2 px-4 me-2">
+                                            <MdOutlineDateRange style={{marginRight:'.3rem'}}  />
+                                            {item.Year}</p>
+                                        <p className="border rounded-pill d-inline py-2 px-4 me-2">
+                                            <FcRating style={{marginRight:'.3rem'}} />
+                                            {item.rate}</p><br></br>
+                                        <button type='button' className='btn btn-danger px-4 py-2 mt-4'>
+                                            <FaRegPlayCircle style={{ fontSize:'1.3rem' ,marginRight:'.3rem'}} />
+                                            Watch</button>
+                                    </div>
+                                </div>
                             </SwiperSlide> 
                         ))}
                     </div>
